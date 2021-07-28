28f2640cb1edea819e737acbb078290d604af33c
Michelle3334 <michelle.toscano1978@gmail.com>
2021-07-28 08:02:58 +0000

    Add model for courses

--- course/models.py
+++ course/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    attendees = models.ManyToManyField(
+        User, related_name='course_attendees', blank=True)
+    min_people = models.IntegerField(default=0)
+    max_people = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.course_name
+
+    def number_of_attendees(self):
+        return self.attendees.count()
