777c568ca8b1ddcbabe4461967ac109e37231583
Michelle3334 <michelle.toscano1978@gmail.com>
2021-07-28 13:24:50 +0000

    Add lesson and module models, add base and index html with basic layout

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Module, Lesson
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -10,4 +10,18 @@ class CourseAdmin(SummernoteModelAdmin):
     list_display = ('course_name', 'slug', 'status', 'created_on')
     search_fields = ['course_name', 'content']
     list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
+@admin.register(Module)
+class ModuleAdmin(SummernoteModelAdmin):
+    list_display = ('module_name', 'created_on')
+    search_fields = ['module_name', 'content']
+    summernote_fields = ('content')
+
+
+@admin.register(Lesson)
+class LessonAdmin(SummernoteModelAdmin):
+    list_display = ('lesson_name', 'created_on')
+    search_fields = ['lesson_name', 'content']
     summernote_fields = ('content')
