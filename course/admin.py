af190ce5353b5d52c6e299a4b1e2ecab41a42700
Michelle3334 <michelle.toscano1978@gmail.com>
2021-07-28 08:50:53 +0000

    Install summernote, create super user and modify admin

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Course
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('course_name',)}
+    list_display = ('course_name', 'slug', 'status', 'created_on')
+    search_fields = ['course_name', 'content']
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
