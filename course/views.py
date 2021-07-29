1dd82c77d725805058c3ea3fed8622f2001e0386
Michelle3334 <michelle.toscano1978@gmail.com>
2021-07-29 11:52:19 +0000

    Add course image and text display to index.html

--- course/views.py
+++ course/views.py
@@ -5,6 +5,6 @@ from .models import Course
 
 class CourseList(generic.ListView):
     model = Course
-    queryset = Course.objects.filter(status=1).order_by('-created_on')
+    queryset = Course.objects.filter(status=1)
     template_name = 'index.html'
     paginate_by = 3
