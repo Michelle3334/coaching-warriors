5d99b4b0f0d93d61a41ddf65f192b76ad97723ef
Michelle3334 <michelle.toscano1978@gmail.com>
2021-08-16 08:49:28 +0000

    Add model for coaches, remove fields from course model

--- coaches/models.py
+++ coaches/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Inactive"), (1, "Active"))
+
+
+class Coach(models.Model):
+    coach_name = models.CharField(
+        primary_key=True, max_length=200, unique=True)
+    email = models.EmailField()
+    profile_image = CloudinaryField('image', default='placeholder')
+    title = models.CharField(max_length=100)
+    about = models.TextField()
+
+    class Meta:
+        ordering = ['coach_name']
+
+    def __str__(self):
+        return self.coach_name
--- course/models.py
+++ course/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -14,14 +13,9 @@ class Course(models.Model):
     image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    min_people = models.IntegerField(default=0)
-    max_people = models.IntegerField(default=0)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.course_name
-
-    def number_of_attendees(self):
-        return self.attendees.count()
