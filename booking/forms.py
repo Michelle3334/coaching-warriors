690c87305bfae5a99b93149485eae2ec1248f012
Michelle3334 <michelle.toscano1978@gmail.com>
2021-10-21 14:27:41 +0000

    Add phone number to booking form

--- booking/forms.py
+++ booking/forms.py
@@ -6,6 +6,6 @@ class BookingForm(forms.Form):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email_address = forms.EmailField(required=True)
-    telephone = forms.IntegerField(required=True)
+    telephone = forms.CharField(max_length=12, required=True)
     date = forms.DateField(required=True, initial=datetime.date.today)
     message = forms.CharField(widget=forms.Textarea, required=False)
