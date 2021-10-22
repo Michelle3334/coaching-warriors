64c2bed02561b9f0d2e5136f1cff50b79ba7f7ef
Michelle3334 <michelle.toscano1978@gmail.com>
2021-10-22 12:42:23 +0000

    Alter view to include course list

--- booking/views.py
+++ booking/views.py
@@ -5,6 +5,7 @@ from course.models import Course
 
 class Booking(TemplateView):
     template_name = 'booking.html'
+    course_list = Course.objects.filter(status=1)
 
     def booking(self, request):
-        return render(request, 'booking.html')
+        return render(request, 'booking.html', {'course_list': course_list})
