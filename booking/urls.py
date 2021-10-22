2f07ee9e548158dd837b9d48ed566548683a5f92
Michelle3334 <michelle.toscano1978@gmail.com>
2021-10-22 08:23:30 +0000

    Amend booking form

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('booking/', views.booking, name='booking'),
+    path('booking/', views.Booking.as_view(), name='booking'),
 ]
