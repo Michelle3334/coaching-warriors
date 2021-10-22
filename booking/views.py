d40c18abb732ca3f2b4c4d85060d886854aabfaf
Michelle3334 <michelle.toscano1978@gmail.com>
2021-10-22 14:26:13 +0000

    Update views to display course list

--- booking/views.py
+++ booking/views.py
@@ -5,7 +5,7 @@ from course.models import Course
 
 class Booking(TemplateView):
     template_name = 'booking.html'
-    course_list = Course.objects.filter(status=1)
 
     def booking(self, request):
+        course_list = Course.objects.filter(status=1)
         return render(request, 'booking.html', {'course_list': course_list})
