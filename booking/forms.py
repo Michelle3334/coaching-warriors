b7d543f7891dc3d7d22e5fa6106a8df06f9ab220
Michelle3334 <michelle.toscano1978@gmail.com>
2021-10-27 09:01:13 +0000

    Change telephone field to widget

--- booking/forms.py
+++ booking/forms.py
@@ -1,13 +1,14 @@
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField
 
 
 class BookingForm(forms.Form):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     email_address = forms.EmailField(required=True)
-    telephone = forms.IntegerField(required=True)
+    telephone = PhoneNumberField(widget=forms.TextInput(), required=True)
     course = forms.CharField()
     coach = forms.CharField()
-    date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))                   
+    date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     additional_information = forms.CharField(
         widget=forms.Textarea, required=False)
