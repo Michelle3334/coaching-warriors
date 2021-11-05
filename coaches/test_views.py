b7e8044f2f515f458d45cd926ddbec052f2d58b0
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-05 13:58:38 +0000

    Add test for view

---
+++ coaches/test_views.py
@@ -0,0 +1,11 @@
+"""Coaches app view testcases"""
+from django.test import TestCase
+
+
+class TestCoachListViews(TestCase):
+    """Test Coach list view"""
+    def test_coach_list(self):
+        """Test Coach list view"""
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
