209129f70b761e6ae38107a924813635b8aa1a6e
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-06 12:59:42 +0000

    Add more fields to models test

--- coaches/test_models.py
+++ coaches/test_models.py
@@ -8,5 +8,8 @@ class TestModels(TestCase):
     def test_coach_string_method_returns_coach(self):
         """Test string is returned"""
         item = Coach.objects.create(
-            coach_name='Test coach', status='1')
+            coach_name='Test coach',
+            email='test@gmail.com',
+            title="Test Title",
+            status='1')
         self.assertEqual(str(item), 'Test coach')
