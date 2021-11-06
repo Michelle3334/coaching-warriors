f58d8d157ebab2b0b3ab09b9f70acce7a08c3684
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-06 12:26:58 +0000

    Add tests for views

--- course/test_views.py
+++ course/test_views.py
@@ -1,5 +1,6 @@
 """Course app view testcases"""
 from django.test import TestCase
+from .models import Course
 
 
 class TestCourseListViews(TestCase):
@@ -9,3 +10,32 @@ class TestCourseListViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+
+class TestCourseDetailViews(TestCase):
+    """Test Course detail view"""
+    def test_course_detail(self):
+        """Test Course detail view"""
+        item = Course.objects.create(
+            course_name='Test course', slug='test_course', status='1')
+        response = self.client.get(f'/slug:slug/{item.slug}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'course_detail.html')
+
+
+class TestGalleryViews(TestCase):
+    """Test Gallery view"""
+    def test_gallery(self):
+        """Test Gallery view"""
+        response = self.client.get('/gallery/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'gallery.html')
+
+
+class TestAboutViews(TestCase):
+    """Test About view"""
+    def test_about(self):
+        """Test About view"""
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
