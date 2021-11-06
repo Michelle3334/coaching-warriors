5788c7ef2b379c4bb9ff6c85921a7ea04383d19b
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-06 13:10:32 +0000

    Add tests for models in course app

---
+++ course/test_models.py
@@ -0,0 +1,14 @@
+"""Course model testing"""
+from django.test import TestCase
+from .models import Course
+
+
+class TestModels(TestCase):
+    """Testing course model"""
+    def test_course_string_method_returns_course(self):
+        """Test string is returned"""
+        item = Course.objects.create(
+            course_name='Test course',
+            slug='test_course',
+            status='1')
+        self.assertEqual(str(item), 'Test course')
