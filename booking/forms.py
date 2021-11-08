3b44ea343d01690d6ce856d2b6a166d7f73aa5be
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-08 10:21:22 +0000

    Change wording for message to user

--- booking/forms.py
+++ booking/forms.py
@@ -17,6 +17,7 @@ class BookingForm(forms.Form):
     coach = forms.ModelChoiceField(
         queryset=Coach.objects.filter(status=1),
         empty_label="(Please select a coach)")
-    date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+    requested_date = forms.DateField(
+        widget=forms.TextInput(attrs={'type': 'date'}))
     additional_information = forms.CharField(
         widget=forms.Textarea, required=False)
