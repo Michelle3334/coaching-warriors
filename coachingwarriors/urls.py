eb202be9348d91cfd9296e0fb401f49234ccaf19
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-08 08:04:35 +0000

    Change position of summernote path

--- coachingwarriors/urls.py
+++ coachingwarriors/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
     path('', include('coaches.urls'), name='coaches_urls'),
     path('', include('booking.urls'), name='booking_urls'),
     path('', include('contactemail.urls'), name='contact_urls'),
     path('', include('course.urls'), name='course_urls'),
+    path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
 ]
