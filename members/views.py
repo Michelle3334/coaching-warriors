022c4230bed0ae133a1a05683c626bde8603f5ba
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-09 13:59:46 +0000

    Debug error causing page 404 error

--- members/views.py
+++ members/views.py
@@ -1,15 +1,11 @@
 """Views for Member profile view"""
 from django.shortcuts import render
+from django.views import generic, View
 from django.views.generic import TemplateView
 # from django.contrib import messages
 # from django.urls import reverse_lazy
 from .forms import ProfileForm
 
-
-# class MemberViewProfile(generic.CreateView):
-# form = ProfileForm
-#    template_name = 'profile.html'
-#    success_url = reverse_lazy('profile')
 
 class MemberViewProfile(TemplateView):
     """Member profile page view"""
