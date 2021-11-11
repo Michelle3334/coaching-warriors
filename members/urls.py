6367517e47e6eeb84cc47d26c718b9d00c0cb86a
Michelle3334 <michelle.toscano1978@gmail.com>
2021-11-11 14:49:02 +0000

    Add change password template and wire up url

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,11 @@
 """URL paths for members app"""
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
     path('members/', views.MemberViewProfile.as_view(), name='members'),
+    path('password/', auth_views.PasswordChangeView.as_view(
+        template_name='change_password.html')),
 ]
